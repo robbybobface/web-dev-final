249431760947429654b4675191b52fe991620843
Jarrett Anderson <jarrett2000@gmail.com>
2022-04-21 15:16:56 -0400

    testing

--- src/services/album-service.js
+++ src/services/album-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:4000/api' || process.env.WEB_DEV_FINAL_API_BASE;
+const API_BASE = process.env.WEB_DEV_FINAL_API_BASE || 'http://localhost:4000/api';
 const ALBUMS_API = `${API_BASE}/albums`;
 
 export const findAllAlbums = async () => {
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = 'http://localhost:4000/api' || process.env.WEB_DEV_FINAL_API_BASE;
+const API_BASE = process.env.WEB_DEV_FINAL_API_BASE || 'http://localhost:4000/api';
 const SECURITY_API = `${API_BASE}/auth`;
 
 const api = axios.create({
