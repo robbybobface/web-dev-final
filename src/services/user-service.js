9f0f4cea722e0c984e83b674fef2c89266651c67
Jarrett Anderson <jarrett2000@gmail.com>
2022-04-21 17:41:14 -0400

    testing

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -4,38 +4,38 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 const USERS_API = `${API_BASE}/users`;
 
 export const createUser = async (user) => {
-    const response = await axios.post(USERS_API, user);
+    const response = await axios.post(USERS_API, user, { withCredentials: true });
     return response.data;
 };
 
 export const findAllUsers = async (search) => {
     if (search) {
-        const response = await axios.get(`${USERS_API}?${search}`);
+        const response = await axios.get(`${USERS_API}?${search}`, { withCredentials: true });
         return response.data;
     } else {
-        const response = await axios.get(`${USERS_API}${search}`);
+        const response = await axios.get(`${USERS_API}${search}`, { withCredentials: true });
         return response.data;
     }
 };
 
 export const deleteUser = async (user) => {
     const response = await axios
-        .delete(`${USERS_API}/${user._id}`);
+        .delete(`${USERS_API}/${user._id}`, { withCredentials: true });
     return response.data;
 };
 export const updateUser = async (user) => {
     const response = await axios
-        .put(`${USERS_API}/${user._id}`, user);
+        .put(`${USERS_API}/${user._id}`, user, { withCredentials: true });
     return response.data;
 };
 
 export const findUserByUsername = async (username) => {
-    const response = await axios.get(`${USERS_API}/${username}`);
+    const response = await axios.get(`${USERS_API}/${username}`, { withCredentials: true });
     return response.data;
 };
 
 export const findUserByEmail = async (email) => {
-    const response = await axios.get(`${USERS_API}/${email}`);
+    const response = await axios.get(`${USERS_API}/${email}`, { withCredentials: true });
     return response.data;
 };
 
